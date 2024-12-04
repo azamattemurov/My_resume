6cbcd23cc4f61e591aea6a6799a6011ae1a514fc
user <sotvoldiyevazamat193@gmail.com>
2024-12-04 16:03:53 +0500

    newwsklllkfdsssdzllssasdvjbsaddasdddds

--- resume/models.py
+++ resume/models.py
@@ -10,6 +10,7 @@ class Song(models.Model):
     album_thumbnail = models.ImageField(upload_to='album_thumbnails/')
     audio_file = models.FileField(upload_to='songs/')
     download_count = models.IntegerField(default=0)
+    AUTH_USER_MODEL = 'auth.User'
 
     def __str__(self):
         return self.song_name
